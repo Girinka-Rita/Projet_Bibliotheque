01bf1cb07b17b3cdda6aee4830eb5d4cefaf0ec0
Rita <ritamartinegirinka@gmail.com>
2026-04-17 11:20:48 +0200

    first commit

---
+++ commun/footer.php
@@ -0,0 +1,21 @@
+<footer class="bg-white text-black py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="/Promo321/Projet_Bibliothéque/img/logocfa.png" alt="Logo École" height="50" width="125" margin_bottom="30px" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <h5>Bibliotheque Cléry</h5>
+                <p>Club multi-sports : basketball, tennis, volleyball.</p>
+                <p>© 2026 Tous droits réservés.</p>
+            </div>
+            <div class="col-md-4 text-md-start">
+                <p><strong>Contact :</strong> 01 23 45 67 89</p>
+                <p><strong>E-mail: </strong> contact@clubsportif.fr</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Bibliothèque Cléry</title>
+  <meta content="Bienvenue à la Bibliothèque Cléry" name="description">
+  <meta content="Bibliothéque Cléry" name="keywords">
+
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"><header id="header" class="header dark-background d-flex flex-column">
+    <i class="header-toggle d-xl-none bi bi-list"></i>
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,8₀;₀,9₀;1,1₀;1,2₀;1,3₀;1,4₀;1,5₀;1,6₀;1,7₀;1,8₀;1,9₀&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+    <header id="header" class="header dark-background d-flex flex-column">
+    <i class="header-toggle d-xl-none bi bi-list"></i>
+
+    <div class="profile-img">
+      <img src="assets/img/mon-profile-img.jpg" alt="" class="img-fluid rounded-circle">
+    </div>
+
+    <a href="index.html" class="logo d-flex align-items-center justify-content-center">
+      <!-- Uncomment the line below if you also wish to use an image logo -->
+      <!-- <img src="assets/img/logo.png" alt=""> -->
+      <h1 class="sitename">Bibliothèque Cléry</h1>
+    </a>
+
+    <nav id="navmenu" class="navmenu">
+      <ul>
+        <li><a href="#accueil class="active"><i class="bi bi-house navicon"></i>Accueil</a></li>
+        <li><a href="#introduction"><i class="bi bi-person navicon"></i> A propos</a></li>
+        <li><a href="#livres"><i class="bi bi-file-earmark-text navicon"></i> Livres</a></li>
+        <li><a href="#auteur"><i class="bi bi-images navicon"></i> Auteur</a></li>
+        <li><a href="#contact"><i class="bi bi-envelope navicon"></i> Contacts</a></li>
+
+      </ul>
+    </nav>
+
+  </header>
